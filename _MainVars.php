419caeadc413ad39492fd41319738bb17bc6d9b6
Peter Zieseniss <peter@zconsulting.net>
2017-05-22 16:24:13 +0200

    Update _MainVars.php

--- _MainVars.php
+++ _MainVars.php
@@ -1,8 +1,8 @@
 <?php
 
-$YourPageURL='xxxxxx'; // example: my.domain.tld/my/dir/  don't forget the trailing slash!
-$YourProtocol='xxxxxx'; // https or http
-$YourUA='UA-xxxxxxxx-x'; // Google Analytics ID
+$YourPageURL='xxxxxx'; // example: my.domain.tld/my/dir/  **don't forget the trailing slash!** -- missingnpr.zconsulting.net/
+$YourProtocol='xxxxxx'; // https or http -- https
+$YourUA='UA-xxxxxxxx-x'; // Google Analytics ID -- UA-24869005-2
 
 
 if(isset($_SERVER['HTTPS'])){$protocol='https://'; }else{$protocol='http://'; }
@@ -25,11 +25,5 @@ else {
 	}
 
 $FullPageURL=$protocol.'://'.$YourPageURL; 
-//$FullAppURL=$protocol.$AppURL; 
-	
-//echo '<br>The App domain: '.rtrim($PageURL,'/').'<br>'; 
-//echo '<br>The App URL: '.$TheAppURL.'<br>'; 	
-//echo '<br>The Full URL: '.$TheAppURL.$TheLastBit.'<br>'; 
-
 
 ?>
